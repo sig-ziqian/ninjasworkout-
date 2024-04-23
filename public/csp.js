3dd4581c9908b203e93119494786066b09e56487
Ziqian Guo <ziqian@synopsys.com>
2024-04-23 12:59:48 -0600

    fix upload

--- public/csp.js
+++ public/csp.js
@@ -10,7 +10,11 @@ const dbConfig = require('./database/db');
 var cookieparser =require('cookie-parser');
 const csurf = require('csurf');
 var multer = require('multer');
-var upload = multer();
+
+var upload = multer({
+  limits: { fileSize: 1024*1024 }
+});
+
 const rateLimit = require("express-rate-limit");
 const fileupload = require("express-fileupload");
 //app.use('/images', express.static(__dirname + '/images'));
